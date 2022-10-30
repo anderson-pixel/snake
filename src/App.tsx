0574339538b79215ab16fb3ccaced562f7d463a9
anderson <anderson.ap133@gmail.com>
2022-10-30 13:57:44 -0300

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <>
+      <div className="App">
+      <canvas id='game' width='440' height='440'></canvas>
+      </div>
+      <h1 className="">Snake Game</h1>
+      <p>W A S D / Arrow Keys for Movement</p>
+      <button onClick={() => window.location.reload()}>Press R to Restart</button>
+      <a href='https://github.com/anderson-pixel' style={{position: 'absolute', zIndex: '10',
+  color: 'cyan',
+  left: '20px', top: '550px', 
+  margin: '0', fontSize: '14.5px', wordBreak: 'break-word', maxWidth: '150px'}}>Made By Anderson Pereira 2022
+  </a>
+      </>
+    );
 }
 
 export default App;
