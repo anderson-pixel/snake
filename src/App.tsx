bf83fb832a1d6b8f69c64b89ab6bf865e1016f4f
anderson <anderson.ap133@gmail.com>
2023-01-10 22:01:06 -0300

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,6 @@ function App() {
       <h1 className="">Snake Game</h1>
       <p>W A S D / Arrow Keys for Movement</p>
       <button onClick={() => window.location.reload()}>Press R to Restart</button>
-      <a href='https://github.com/anderson-pixel' style={{position: 'absolute', zIndex: '10',
-  color: 'cyan',
-  left: '20px', top: '550px', 
-  margin: '0', fontSize: '14.5px', wordBreak: 'break-word', maxWidth: '150px'}}>Made By Anderson Pereira 2022
-  </a>
     </>
   );
 }
